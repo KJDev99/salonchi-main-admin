e8912e2772ad1072b7cd4cfc46dbe799d10cf099
Jonibek <munirovjonibek@gmail.com>
2024-11-11 17:16:06 +0500

    some changes

--- src/pages/category-order/index.jsx
+++ src/pages/category-order/index.jsx
@@ -1,86 +1,11 @@
-import { request } from "@/shared/api/request";
-import { DragSortTable } from "@ant-design/pro-components";
-import { message } from "antd";
-import { useEffect, useState } from "react";
+import Draggable from "@/components/draggable";
 
-const columns = [
-  {
-    title: "",
-    dataIndex: "sort",
-    width: 150,
-    className: "drag-visible",
-  },
-  {
-    title: "ID",
-    dataIndex: "key",
-    width: 150,
-    className: "drag-visible",
-  },
-  {
-    title: "Kategoriya nomi",
-    dataIndex: "name",
-    className: "drag-visible",
-  },
-];
-
-const CategoryOrders = () => {
-  const [dataa, setDataa] = useState([]);
-  const [dataSource, setDataSource] = useState([...dataa]);
-  useEffect(() => {
-    const getData = async () => {
-      const response = await request.get("/category/list");
-      setDataa(response.data);
-      setDataSource(
-        response.data.map((item) => ({
-          key: item.id.toString(),
-          name: item.name_uz,
-        }))
-      );
-    };
-    getData();
-  }, []);
-  const handleDragSortEnd = async (beforeIndex, afterIndex, newDataSource) => {
-    console.log(beforeIndex, afterIndex, dataSource.length);
-    console.log(
-      "self",
-      dataSource[beforeIndex],
-      "bitta oldingi",
-      dataSource[
-        afterIndex === dataSource.length - 2
-          ? dataSource.length - 2
-          : afterIndex - 1
-      ]?.key
-    );
-    setDataSource(newDataSource);
-    const response = await request.post(
-      "admin/category/" + dataSource[beforeIndex]?.key + "/ordering",
-      {
-        above_category:
-          dataSource[
-            afterIndex === dataSource.length - 2
-              ? dataSource.length - 2
-              : afterIndex - 1
-          ]?.key,
-      }
-    );
-    if (response.status === 200) {
-      message.success("O'zgartirildi");
-    } else {
-      message.error("Xatolik");
-    }
-  };
-
+const CategoryOrder = () => {
   return (
-    <DragSortTable
-      headerTitle="Kategoriyalarni ketma-ketligini o'rgartirish"
-      columns={columns}
-      rowKey="key"
-      search={false}
-      pagination={false}
-      dataSource={dataSource}
-      dragSortKey="sort"
-      onDragSortEnd={handleDragSortEnd}
-    />
+    <div>
+      <Draggable />
+    </div>
   );
 };
-export default CategoryOrders;
+
+export default CategoryOrder;

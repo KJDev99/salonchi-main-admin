1b840c5587d4edb9387a8069887672b8d3a3ea55
Omonov <algoritm1202@gmail.com>
2025-04-08 10:14:07 +0500

    vite2

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
-import { defineConfig } from "vite";
+/* import { defineConfig } from "vite";
 import svgr from "vite-plugin-svgr";
 import react from "@vitejs/plugin-react";
 import checker from "vite-plugin-checker";
@@ -9,6 +9,29 @@ export default defineConfig({
     port: 3002,
     // open: true,
     open: false,
+  },
+
+  plugins: [
+    jsconfigPaths(),
+    svgr(),
+    react(),
+    checker({
+      javascript: true,
+    }),
+  ],
+}); */
+
+import { defineConfig } from "vite";
+import svgr from "vite-plugin-svgr";
+import react from "@vitejs/plugin-react";
+import checker from "vite-plugin-checker";
+import jsconfigPaths from "vite-jsconfig-paths";
+
+export default defineConfig({
+  server: {
+    port: 3002,
+    open: true,
+    allowedHosts: ["admin.salonchi.uz"], // Ruxsat etilgan hostlarni qo'shish
   },
 
   plugins: [
